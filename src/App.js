247153294b9b91937785390a49103c2bba402ff9
Liliana Leyva <ingeleyva@yahoo.com>
2021-02-07 21:10:14 -0600

    it is working

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import React, {useEffect, useState} from 'react';
-import Character from './components/Character'
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+//importing character.js
+import Character from './components/Character';
+//importing styled to make the site pretty
 import styled from 'styled-components';
 
+//styling h1, putting the text on the center of the page.
 const H1 = styled.h1`
   text-align: center;
   font-family: franklin gothic;
-  color: gold;
-  font-size: 60px;
+  color: #FFE81F;
+  font-size: 100px;
 `;
 
 const AppDiv = styled.div`
-  color: blue;
-   
 `;
-
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+
+//setting the state properties.
+  const [characters, setCharacters] = useState([])
+  
+  
+  
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
- 
-    const [characters, setCharacters] = useState([]);
-    
-    useEffect(() => {
-        axios.get('https://swapi.dev/people')
-        .then(response => {
-            console.log(response.data.results);
-            setCharacters(response.data.results)
-        })
-        .catch(error => console.log(error))
-        
-    }, [])
-    return (
-  
-      <AppDiv className="App">
-        <H1 className="Header">React Wars</H1>
-        <Character characters={characters}/>
-      </AppDiv>
-    );
+
+  useEffect(() => {
+  //making the api request and getting the response
+    axios.get('https://swapi.py4e.com/api/people')
+    .then(res => {
+      console.log(res.data.results)
+      setCharacters(res.data.results)})
+    .catch(err => console.log(err));
+  }, [])
+  return (
+    //rendering the api response
+    <AppDiv className="App">
+      <H1 className="Header">React Wars</H1>
+      <Character characters={characters}/>
+    </AppDiv>
+  );
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,32 +1,35 @@
 // Write your Character component here
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-
+//styling the divs 
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    
 `;
 
+//styling the information we are going to display
 const StyledH2 = styled.h2`
-    border: solid green 2px;
-    padding: 8px 8px;
+    border-radius:12px;
+    padding: 15px 15px;
     text-align: center;
-    color: #F4F2C9;
-    background-color: #265999 ;
+    color: white;
+    font-size: 30px;
+    background-color: black;
 `;
 
+//looping over the api response and showing on screen the character name, gender and birth year
 function Character(props) {
     return (
         <StyledDiv>
         <div>{props.characters.map(char => {
-            return <StyledH2 key={char.name}>{char.name} {char.birth_year}</StyledH2>
+            return <StyledH2 key={char.name}>{char.name}{" "}{char.gender} { " "} {char.birth_year}</StyledH2>
         })}
         </div>
         </StyledDiv>
     )
-    //
 }
 
 export default Character;
