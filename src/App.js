cac7de3868b0e7e5270bd9b801516cfd5d59ed28
Liliana Leyva <ingeleyva@yahoo.com>
2021-02-07 20:07:43 -0600

    trying to make characters work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Character from './components/Character'
+import axios from 'axios';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  text-align: center;
+  font-family: franklin gothic;
+  color: gold;
+  font-size: 60px;
+`;
+
+const AppDiv = styled.div`
+  color: blue;
+   
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,12 +22,25 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+ 
+    const [characters, setCharacters] = useState([]);
+    
+    useEffect(() => {
+        axios.get('https://swapi.dev/people')
+        .then(response => {
+            console.log(response.data.results);
+            setCharacters(response.data.results)
+        })
+        .catch(error => console.log(error))
+        
+    }, [])
+    return (
+  
+      <AppDiv className="App">
+        <H1 className="Header">React Wars</H1>
+        <Character characters={characters}/>
+      </AppDiv>
+    );
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,49 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+// const Character = (props) => {
+
+    
+//     return(
+//         <div>
+//             {props.characters.map(item => {
+//             return <h2 key={item.name}>{item.name} {item.gender}{item.birth_year}</h2>
+//             })}
+//         </div>
+//     ) 
+// }
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const StyledH2 = styled.h2`
+    border: solid green 2px;
+    padding: 8px 8px;
+    text-align: center;
+    color: #F4F2C9;
+    background-color: #265999 ;
+`;
+
+function Character(props) {
+    return (
+        <StyledDiv>
+        <div>{props.characters.map(char => {
+            return <StyledH2 key={char.name}>{char.name} {char.birth_year}</StyledH2>
+        })}
+        </div>
+        </StyledDiv>
+    )
+}
+
+export default Character;
+
+
+
+
+
+
+
